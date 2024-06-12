83798ad2a870a0658526c4f4043eee4933fcac4a
aidqen <idanmarkin8@gmail.com>
2024-06-12 16:35:12 +0300

    Add remove and get functionality to the backend server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
 
 const app = express()
 
@@ -6,10 +7,18 @@ app.get('/', (req, res) => res.send('Hello there'))
 app.listen(3030, () => console.log('Server ready at port 3030'))
 
 app.get('/api/bug', (req, res) => {
-    res.send('hi')
+  bugService.query().then(cars => res.send(cars))
 })
 app.get('/api/bug/save', (req, res) => {
-    res.send('<h1>Res</h1>')
+  res.send('<h1>Res</h1>')
 })
-app.get('/api/bug/:bugId', (req, res) => {})
-app.get('/api/bug/:bugId/remove', (req, res) => {})
+app.get('/api/bug/:bugId', (req, res) => {
+  const { bugId } = req.params
+  console.log(bugId)
+  bugService.getById(bugId).then(bug => res.send(bug))
+})
+app.get('/api/bug/:bugId/remove', (req, res) => {
+  const { bugId } = req.params
+  bugService.remove(bugId)
+            .then(() => res.send(`Bug ${bugId} deleted`))
+})
