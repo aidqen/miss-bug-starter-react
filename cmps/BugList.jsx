9638dd6c871b9ede35d648e4f61f9acb29bbd7e1
aidqen <idanmarkin8@gmail.com>
2024-06-13 15:09:57 +0300

    Fix BugPreview article error

--- cmps/BugList.jsx
+++ cmps/BugList.jsx
@@ -1,6 +1,6 @@
 const {Link} = ReactRouterDOM
 
-import {BugPreview} from './BugPreview.jsx'
+import { BugPreview } from './BugPreview.jsx'
 
 export function BugList({bugs, onRemoveBug, onEditBug}) {
   return (
--- cmps/BugPreview.jsx
+++ cmps/BugPreview.jsx
@@ -1,10 +1,9 @@
 
 
 export function BugPreview({bug}) {
-
-    return <artice>
+    return <article>
         <h4>{bug.title}</h4>
         <h1>🐛</h1>
         <p>Severity: <span>{bug.severity}</span></p>
-    </artice>
+    </article>
 }
