82afbbf9e82b589cb726f05a48c805826ac43ba2
aidqen <idanmarkin8@gmail.com>
2024-06-25 15:45:06 +0300

    Make new pageCount func

--- server.js
+++ server.js
@@ -14,26 +14,6 @@ app.listen(3030, () =>
 )
 
 app.get('/api/bug', (req, res) => {
-  if (req.query.bugId) {
-    const bugId = req.query.bugId
-
-    var visitedBugs = req.cookies.visitedBugs || []
-    console.log(req.cookies.visitedBugs)
-
-    if (visitedBugs.length >= 3)
-      return res.status(401).send('You have already visited 3 bugs.')
-
-    if (!visitedBugs.includes(bugId)) visitedBugs.push(bugId)
-    res.cookie('visitedBugs', visitedBugs, { maxAge: 10000 })
-
-    return bugService.getById(bugId)
-      .then(bug => res.send(bug))
-      .catch(err => {
-        res.status(500).send('error')
-        throw err
-      })
-  }
-
   const { txt, minSeverity, sortBy, pageIdx } = req.query
   const filterBy = {
     txt,
@@ -43,6 +23,14 @@ app.get('/api/bug', (req, res) => {
   }
   bugService.query(filterBy).then(bugs => res.send(bugs))
 })
+
+app.get('/api/bug/pageCount', (req, res) => {
+  bugService.getPageCount()
+  .then(pages => res.send(pages + ''))
+})
+
+
+
 
 app.post('/api/bug', (req, res) => {
   console.log(req.body);
@@ -58,8 +46,27 @@ app.post('/api/bug', (req, res) => {
   bugService.save(bugToSave).then(savedBug => res.send(savedBug))
 })
 
-app.delete('/api/bug', (req, res) => {
-    const bugId = req.query.bugId
+app.get('/api/bug/:bugId', (req, res) => {
+  const { bugId } = req.params
+
+  var visitedBugs = req.cookies.visitedBugs || []
+
+  if (visitedBugs.length >= 3)
+    return res.status(401).send('You have already visited 3 bugs.')
+
+  if (!visitedBugs.includes(bugId)) visitedBugs.push(bugId)
+  res.cookie('visitedBugs', visitedBugs, { maxAge: 10000 })
+
+  return bugService.getById(bugId)
+    .then(bug => res.send(bug))
+    .catch(err => {
+      res.status(500).send('error')
+      throw err
+    })
+})
+
+app.delete('/api/bug/:bugId', (req, res) => {
+    const {bugId} = req.params
 
     console.log(bugId);
   
