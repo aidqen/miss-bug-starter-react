38a5d7a0605707a7c500e205e51b994635f70987
aidqen <idanmarkin8@gmail.com>
2024-06-30 14:46:19 +0300

    Made LoginSignup more dynamic and functional

---
+++ public/cmps/LoginSignup.jsx
@@ -0,0 +1,93 @@
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+import { userService } from '../services/user.service.js'
+import { Login } from './Login.jsx'
+import { Signup } from './Signup.jsx'
+
+const { useState } = React
+
+export function LoginSignup({ setUser }) {
+  const [loginOrSignup, setLoginOrSignup] = useState('login')
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+    fullName: '',
+  })
+
+  const isLogin = loginOrSignup === 'login'
+  const { username, password, fullName } = credentials
+
+  function handleChange({ target }) {
+    const { name: field, value } = target
+    setCredentials(prevCreds => ({ ...prevCreds, [field]: value }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    isLogin ? login(credentials) : signup(credentials)
+  }
+
+  function login(credentials) {
+    userService
+      .login(credentials)
+      .then(setUser)
+      .then(() => {
+        showSuccessMsg(`Successfully logged into ${fullName}`)
+      })
+      .catch(err => showErrorMsg('Oops try again'))
+    }
+    
+    function signup(credentials) {
+      userService
+      .signup(credentials)
+      .then(setUser)
+      showSuccessMsg(`Successfully Signed Up ${fullName}`)      
+      .catch(err => showErrorMsg('Oops try again'))
+  }
+
+  function toggleLogin(ev) {
+    ev.preventDefault()
+    setLoginOrSignup(isLogin ? 'signup' : 'login')
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="login-signup-form flex flex-column"
+    >
+      {!isLogin && (
+        <React.Fragment>
+          <label htmlFor="fullName">Full Name:</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={fullName}
+            onChange={handleChange}
+          />
+        </React.Fragment>
+      )}
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        value={username}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={password}
+        onChange={handleChange}
+      />
+
+      <button type="submit">{isLogin ? 'Login' : 'Signup'}</button>
+      <button onClick={toggleLogin}>
+        {isLogin ? "Don't have a user? Signup" : 'Already have a user? Login'}
+      </button>
+    </form>
+  )
+}
